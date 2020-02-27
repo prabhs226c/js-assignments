1afb1fe9935f15fbd9c0cc39a0f47cba7c9ef1fb
prabhs226c <prabhs226c@gmail.com>
2020-02-27 16:24:48 -0500

    1 folders commit

--- exercise-console_Singh-Prabhjot/js/console.js
+++ exercise-console_Singh-Prabhjot/js/console.js
@@ -1,19 +1,18 @@
 'use strict'
 
-const temperature = 21.3
-const fruits = ['apple', 'pear', 'orange']
-const eighteen = 18
-const isTrue  = true
-const isNull = null
+const TEMPERATURE = 21.3
+const FRUITS = ['apple', 'pear', 'orange']
+const EIGHTEEN = 18
+const IS_TRUE  = true
+const IS_NULL = null
 let ghostVariable
 
 console.log('Yo.')
-console.log(temperature)
-console.log('['+fruits+']')
-console.log('The temperature is',temperature)
-console.log('The Collection of Fruits is ['+fruits+']')
+console.log(TEMPERATURE)
+console.log('['+FRUITS+']')
+console.log('The temperature is',TEMPERATURE)
+console.log('The Collection of Fruits is ['+FRUITS+']')
 console.log('<h1>No HTML Interpreter</h1>')
-console.log(eighteen+' eighteen',isTrue,isNull,ghostVariable)
+console.log(EIGHTEEN+' eighteen',IS_TRUE,IS_NULL,ghostVariable)
 console.log(('This is a group (closed by default)\n'),'group content')
 console.log(('This is a second group\n'),'content of the second group')
-
