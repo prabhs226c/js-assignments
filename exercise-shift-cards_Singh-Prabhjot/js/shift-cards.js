94b1442f633006fc262f8cd9b5d4f2403ac7936e
prabhs226c <prabhs226c@gmail.com>
2020-02-29 20:00:39 -0500

    changed the functions, old code is commented but accurate

--- exercise-shift-cards_Singh-Prabhjot/js/shift-cards.js
+++ exercise-shift-cards_Singh-Prabhjot/js/shift-cards.js
@@ -3,10 +3,9 @@
 const cardStack = document.getElementById('cards')
 const cards = cardStack.getElementsByTagName('img')
 
-function shiftLeft()
-{
 
-    
+    /**
+    *old version I made
     let extraCard = cards[0].getAttribute('src')
     
     for(let i = 0;i<cards.length-1;i++)
@@ -14,11 +13,17 @@ function shiftLeft()
         cards[i].setAttribute('src',cards[i+1].getAttribute('src'))
     }
     cards[cards.length-1].setAttribute('src',extraCard)
-
+    */
+function shiftLeft()
+{
+    cardStack.appendChild(cards[0]);
 }
 
-function shiftRight()
-{
+
+/**
+ * 
+ * old version I made
+ * 
     let extraCard = cards[cards.length-1].getAttribute('src')
     
     for(let i = cards.length-1;i>0;i--)
@@ -26,5 +31,9 @@ function shiftRight()
         cards[i].setAttribute('src',cards[i-1].getAttribute('src'))
     }
     cards[0].setAttribute('src',extraCard)
-
+    */
+function shiftRight()
+{
+    cardStack.insertBefore(cards[cards.length-1],cards[0])
+    
 }
