fb058af40f80fdbb5eb74cbb077379f80288ce88
prabhs226c <prabhs226c@gmail.com>
2020-03-05 14:06:46 -0500

    number excercise

---
+++ exercise-number-image_Singh-Prabhjot/js/number-image.js
@@ -0,0 +1,7 @@
+document.getElementById('numberInput').addEventListener('change',function(){
+    const imageSet = ['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg']
+    const number = this.value
+    const image = document.getElementById('numberDiv').getElementsByTagName('img')[0]
+   
+    image.setAttribute('src','image/'+imageSet[number])
+})
