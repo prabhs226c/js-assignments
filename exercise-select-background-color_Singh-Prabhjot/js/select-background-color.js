7a757f3675301ec3bb840c3a5bc64bcd160b3266
prabhs226c <prabhs226c@gmail.com>
2020-03-05 13:53:23 -0500

    event listener

--- exercise-select-background-color_Singh-Prabhjot/js/select-background-color.js
+++ exercise-select-background-color_Singh-Prabhjot/js/select-background-color.js
@@ -1,6 +1,9 @@
-function setBackground(){
+
+
+document.getElementById('colorSelect').addEventListener('change', function() 
+{
     const COLOR = document.getElementById('colorSelect').value
     const body = document.getElementsByTagName('body')[0]
     body.setAttribute('bgcolor',COLOR)
+});
 
-}
